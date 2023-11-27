bdfb4e7d32cfe02b2ccda19c2d89c01b076ff77f
BetoCochas <oscargio86@hotmail.com>
2023-11-26 19:44:27 -0500

    Vue

--- Vue.js
+++ Vue.js
@@ -2,8 +2,9 @@ const{createApp}=Vue
 createApp({
     data(){
      return{
-        players:["Adrian Ramos","Victor Ibarbo","Edwin Cardona"],
-        numbers:[9,18,10]
+      players:["Adrian Ramos","Victor Ibarbo","Edwin Cardona"],
+numbers:[9,18,10] 
         
      }}
 }).mount("#App")
+ 
