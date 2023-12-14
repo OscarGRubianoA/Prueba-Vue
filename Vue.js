8b8ff34a2d2a917445ccee669ba35abd20df2314
BetoCochas <oscargio86@hotmail.com>
2023-12-13 23:55:27 -0500

    'vue'

--- Vue.js
+++ Vue.js
@@ -5,6 +5,19 @@ createApp({
       players:["Adrian Ramos","Victor Ibarbo","Edwin Cardona","Andres Sarmiento","Josep Escobar","Facundo Suarez","Cristian Barrios"],
       numbers:[9,18,10] 
         
-     }}
+     }},methods(){
+        function histograma(array){
+            let acum = 0
+            for(let i = 0 ; i < array.length ; i++){
+                
+                if(array[i] === array[0]){
+                 acum ++
+                }
+                
+                }console.log($([i]+1)+":"+" "+ $(acum))
+                
+            }
+            histograma(myArray)
+     }
 }).mount("#App")
  
